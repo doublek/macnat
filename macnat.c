13a270848d0934b3521b61bc570e56e1d984a8b3
doublek <karthikkrishnan.r@gmail.com>
2011-06-28 20:52:03 -0700

    Refactor to separate sending and receiving bits.
    Refactor code to separate out the sending and receiving bits. Also make
    sure that the sockaddr_ll structure is populated with only the required
    struct fields for sending or receiving.

--- macnat.c
+++ macnat.c
@@ -33,7 +33,6 @@
 #include <netpacket/packet.h>
 #include <sys/socket.h>
 
-
 char * random_chars(char *dst, int size)
 {
     static const char allowable_chars[] = "1234567890abcdef";
@@ -49,12 +48,122 @@ char * random_chars(char *dst, int size)
     return dst;
 }
 
+void modify_and_send_packet(void *packet)
+{
+    const char pattern[] = "00:00:5e";
+    char dst1[3], dst2[3], dst3[3];
+    char spoofed_mac_addr[18];
+
+    struct sockaddr_ll sdl;
+    struct packet_mreq mreq;
+    int send_sock, ifindex, protocol;
+
+    /* FIXME: Hardcoded... Sigh.... */
+    ifindex = if_nametoindex("eth2");
+    protocol = htons(ETH_P_ALL);
+
+    sdl.sll_family = AF_PACKET;
+    sdl.sll_halen = 6; /* FIXME: Magic number */
+    memcpy(sdl.sll_addr, (ether_aton(spoofed_mac_addr))->ether_addr_octet, ETHER_ADDR_LEN);
+    sdl.sll_ifindex = ifindex;
+    sdl.sll_protocol = 0; /* Not required for sending */
+    sdl.sll_hatype = 0; /* Not required for sending */
+    sdl.sll_pkttype = 0; /* Not required for sending */
+
+    /*
+     * We use 2 sockets because we will (probably) receive packets via one
+     * interface and send via another interface.
+     */
+    if((send_sock = socket(PF_PACKET, SOCK_RAW, protocol)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    mreq.mr_ifindex = ifindex;
+    mreq.mr_type = PACKET_MR_PROMISC;
+
+    if(setsockopt(send_sock, SOL_SOCKET, PACKET_ADD_MEMBERSHIP, (void *)&mreq, sizeof(mreq)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    sprintf(spoofed_mac_addr, "%s:%s:%s:%s", pattern,
+            random_chars(dst1, sizeof(dst1)),
+            random_chars(dst2, sizeof(dst2)),
+            random_chars(dst3, sizeof(dst3)));
+
+    printf("Should modify and send packet using MAC: %s... \n"
+            "Doing nothing for now...\n", spoofed_mac_addr);
+}
+
 void print_mac_address(unsigned char *addr)
 {
     int i;
     printf("%02x", addr[0]);
     for(i=1; i<ETH_ALEN; ++i)
         printf(":%02x", addr[i]);
+}
+
+void receive_packet()
+{
+    struct sockaddr_ll sdl;
+    struct packet_mreq mreq;
+    int recv_sock, ifindex, protocol;
+
+    unsigned char buffer[ETH_FRAME_LEN];
+    struct ethhdr *hdr;
+
+    /* FIXME: Hardcoded... Sigh...*/
+    ifindex = if_nametoindex("eth2");
+    protocol = htons(ETH_P_ALL);
+
+    sdl.sll_family = AF_PACKET;
+    sdl.sll_protocol = protocol;
+    sdl.sll_ifindex = ifindex;
+    sdl.sll_hatype = 0; /* Will be filled for us */
+    sdl.sll_pkttype = 0; /* Will be filled for us */
+    /* sdl.sll_halen and sdl.sll_addr not required for receiveing */
+
+    if((recv_sock = socket(PF_PACKET, SOCK_RAW, protocol)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    mreq.mr_ifindex = ifindex;
+    mreq.mr_type = PACKET_MR_PROMISC;
+
+    if(setsockopt(recv_sock, SOL_SOCKET, PACKET_ADD_MEMBERSHIP, (void *)&mreq, sizeof(mreq)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+#if 0
+    /* TODO: Nice value for buf. */
+    if(setsockopt(recv_sock, SOL_SOCKET, SO_RCVBUF , NULL, 0) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+#endif
+
+    if(bind(recv_sock, (struct sockaddr *)&sdl, sizeof(sdl)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    while (1) {
+        if (read(recv_sock, buffer, ETH_FRAME_LEN) == -1) {
+            perror("read");
+            exit(1);
+        }
+
+        printf("Got Data:\n");
+        hdr = (struct ethhdr *)buffer;
+        print_mac_address(hdr->h_source);
+        printf(" --> ");
+        print_mac_address(hdr->h_dest);
+        printf("\n");
+        modify_and_send_packet(buffer);
+    }
 }
 
 void usage()
@@ -68,65 +177,15 @@ void usage()
 
 int main(int argc, char *argv[])
 {
-    const char pattern[] = "00:00:5e";
-    char dst1[3], dst2[3], dst3[3];
-    char spoofed_mac_addr[18];
-
-    struct sockaddr_ll sdl;
-    struct packet_mreq mreq;
-    int sock, ifindex, protocol;
-
-    unsigned char buffer[ETH_FRAME_LEN];
-    struct ethhdr *hdr;
-
     srand(time(NULL));
-    sprintf(spoofed_mac_addr, "%s:%s:%s:%s", pattern,
-            random_chars(dst1, sizeof(dst1)),
-            random_chars(dst2, sizeof(dst2)),
-            random_chars(dst3, sizeof(dst3)));
-    printf("MAC: %s\n", spoofed_mac_addr);
 
-    ifindex = if_nametoindex("eth2");
-    printf("ifindex: %d\n", ifindex);
+    /* TODO: Event based code goes here.
+     *  Things I have thought of for now include:
+     *      1. Sock read.
+     *      2. Sock write.
+     * Until then just call receive_packet which does an infinite loop. :-|
+     */
+    receive_packet();
 
-    protocol = htons(ETH_P_ALL);
-
-    sdl.sll_family = AF_PACKET;
-    sdl.sll_protocol = protocol;
-    sdl.sll_ifindex = ifindex;
-    sdl.sll_hatype = ARPHRD_ETHER;
-    sdl.sll_pkttype = PACKET_OUTGOING;
-    sdl.sll_halen = 6; /* FIXME: Magic number */
-    memcpy(sdl.sll_addr, (ether_aton(spoofed_mac_addr))->ether_addr_octet, ETHER_ADDR_LEN);
-
-    if((sock = socket(PF_PACKET, SOCK_RAW, protocol)) == -1) {
-        perror("socket");
-        return -1;
-    }
-
-    mreq.mr_ifindex = ifindex;
-    mreq.mr_type = PACKET_MR_PROMISC;
-
-    if(setsockopt(sock, SOL_SOCKET, PACKET_ADD_MEMBERSHIP, (void *)&mreq, sizeof(mreq)) == -1) {
-        perror("setsockopt");
-        return -1;
-    }
-
-    if(bind(sock, (struct sockaddr *)&sdl, sizeof(sdl)) == -1) {
-        perror("bind");
-        return -1;
-    }
-
-    while (1) {
-        if (read(sock, buffer, ETH_FRAME_LEN) == -1) {
-            perror("read");
-            return -1;
-        }
-        printf("Got Data:\n", buffer);
-        hdr = (struct ethhdr *)buffer;
-        print_mac_address(hdr->h_source);
-        printf(" --> ");
-        print_mac_address(hdr->h_dest);
-        printf("\n");
-    }
+    return 0;
 }
